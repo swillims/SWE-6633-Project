ed7ce041617eaf5ac4d8d42cffb451ca1a190cac
Adam Salam <karnakkid@proton.me>
2025-12-04 20:41:13 -0500

    #10 Adding new projects to list

---
+++ ProjectManagementGui/ProjectManagementGui/ViewModels/NewProjectPopupViewModel.cs
@@ -0,0 +1,69 @@
+using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows.Input;
+using ReactiveUI;
+
+namespace ProjectManagementGui.ViewModels;
+
+public class NewProjectPopupViewModel(Action action) : ViewModelBase
+{
+    private string _projectName = string.Empty;
+    private string _projectOwner = string.Empty;
+    private bool _isVisible;
+
+    private Action? CreateProject { get; set; } = action;
+
+    public bool IsVisible
+    {
+        get => _isVisible;
+        set => this.RaiseAndSetIfChanged(ref _isVisible, value);
+    }
+
+    public string ProjectName
+    {
+        get => _projectName;
+        set
+        {
+            if (_projectName != value)
+            {
+                this.RaiseAndSetIfChanged(ref _projectName, value);
+            }
+        }
+    }
+
+    public string ProjectOwner
+    {
+        get => _projectOwner;
+        set
+        {
+            if (_projectOwner != value)
+            {
+                this.RaiseAndSetIfChanged(ref _projectOwner, value);
+            }
+        }
+    }
+
+    private bool IsValid => !string.IsNullOrWhiteSpace(ProjectName) && 
+                           !string.IsNullOrWhiteSpace(ProjectOwner);
+    public bool DialogResult { get; private set; }
+
+    public void Create()
+    {
+        if (IsValid)
+        {
+            DialogResult = true;
+            CreateProject?.Invoke();
+            
+            ProjectName =  string.Empty;
+            ProjectOwner = string.Empty;
+            IsVisible = false;
+        }
+    }
+
+    public void Cancel()
+    {
+        DialogResult = false;
+        IsVisible = false;
+    }
+}
